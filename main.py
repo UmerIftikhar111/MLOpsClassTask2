c09f5e1245673b461509a03bea50d1ae1e521c01
UmerIftikhar111 <chumeriftikhar11@gmail.com>
2024-03-11 06:45:57 +0500

    trigger jenkins

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ class StudentsInMLOps:
         classname =  "StudentsInMLOps"
         return classname;
 
-# just to check again - trigger jenkins
+# just to check again - trigger jenkins build
 
